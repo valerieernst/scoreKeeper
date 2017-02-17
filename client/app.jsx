faa63b4f94e199b1c67dd31e5618cae9c1ce05a5
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-02-16 22:10:26 -0800

    Move state from leaderboard to app to accomodate adding points to teams

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Leaderboard from './leaderboard.jsx'
+import GivePoints from './givePoints.jsx'
 
-const app = () => (
-  <div>
-    <Leaderboard />
-  </div>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default app;
+    this.state ={
+      scoreData: {}
+    }
+  }
+
+  componentWillMount () {
+    //get score data before component mounts
+    this.getScores();
+  }
+
+  getScores () {
+    //make http request to server
+    axios.get('/scores')
+    .then((data) => {
+      //store score data to component state
+      this.setState({
+        scoreData: data.data
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Leaderboard scoreData={this.state.scoreData}/>
+        <GivePoints />
+      </div>
+    )
+  }
+
+};
--- client/leaderboard.jsx
+++ client/leaderboard.jsx
@@ -1,48 +1,17 @@
 import React, { Component, PropTypes } from 'react';
-import axios from 'axios';
+
 import Team from './team.jsx';
 
 
-export default class Leaderboard extends Component {
-  constructor(props) {
-    super(props);
-
-    //must bind this context
-    this.getScores = this.getScores.bind(this);
-
-    this.state = {
-      teamData: {}
-    };
-  }
-
-  componentWillMount () {
-    //get score data before component mounts
-    this.getScores();
-  }
-
-  getScores () {
-    //make http request to server
-    axios.get('/scores')
-    .then((data) => {
-      //store score data to component state
-      this.setState({
-        teamData: data.data
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    })
-  }
-
-
-  render () {
+const leaderboard = ({ scoreData }) => {
     //iterate through scores object and render each team to screen
-    return (
+  return (
     <div className="leaderboard container"> 
-      {Object.keys(this.state.teamData).map((teamName) => (
-        <Team key={teamName} teamName={teamName} points={this.state.teamData[teamName]} />
-      ))
-      }
+      {Object.keys(scoreData).map((teamName) => (
+        <Team key={teamName} teamName={teamName} points={scoreData[teamName]} />
+      ))}
     </div>
-  )}
+  )
 }
+
+export default leaderboard;
